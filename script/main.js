7f31ec33713fe72fe24f8670a797f50d72ddaa55
KyungSeokNoh <nokyungseok@gmail.com>
2024-07-15 17:47:08 +0900

    first hollys

---
+++ script/main.js
@@ -0,0 +1,22 @@
+$(function(){
+    // 메뉴
+    $(".main > li , .sub_bg").hover(function(){
+        $(".sub").stop().show(500);
+        $(".sub_bg").stop().slideDown("fast");
+    },function(){
+        $(".sub").stop().hide();
+        $(".sub, .sub_bg").stop().slideUp("fast");
+    }) //hover
+
+    //slick
+
+    $(".move").slick({
+        autoplay:true,
+        dots:true,
+        arrows:false,
+        autoplaySpeed:2000,
+        adaptiveHeight:true
+    })
+
+
+})
